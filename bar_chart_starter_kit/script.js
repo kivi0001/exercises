b7bc5fbc0241453ff6bba47bafdfc6893c30e304
Kia <kivi0001@stud.kea.dk>
2026-02-05 12:19:34 +0100

    ufærdig bar chart øvelse

--- bar_chart_starter_kit/script.js
+++ bar_chart_starter_kit/script.js
@@ -1,5 +1,16 @@
 const list = document.querySelector("ul");
+const numberOfCustomersArray = Array.from({length: 20}, () => Math.floor(Math.random() * 100) + 1);
+    
+    const li = document.createElement("li");
+    li.style.setProperty("--height", "30");
+    list.appendChild(li);
 
-const li = document.createElement("li");
-li.style.setProperty("--height", "30");
-list.appendChild(li);
+setInterval(newPillar, 1000);
+
+function newPillar(){
+    numberOfCustomersArray.forEach(Element => {
+        const li = document.createElement("li");
+        li.style.setProperty("--height", Element);
+        list.appendChild(li);
+    });
+}
