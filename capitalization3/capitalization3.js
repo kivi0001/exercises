08b458948a897ce584e4e4e552924318f5319859
Kia <kivi0001@stud.kea.dk>
2026-02-06 11:55:38 +0100

    ekstra capitalization

--- capitalization3/capitalization3.js
+++ capitalization3/capitalization3.js
@@ -1,4 +1,9 @@
-const name = "Margrethe";
+/* const name = "Margrethe";
 const capName = `${name.substring(0, 2)}${name[2].toUpperCase()}${name.substring(3)}`;
+
+console.log(capName); */
+
+const name = "margrethe";
+const capName = `${name[0].toUpperCase()}${name.substring(1)}`;
 
 console.log(capName);
