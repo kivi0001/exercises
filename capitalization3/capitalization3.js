b2fdd0a3b54e1729b88d84d34b837a41cba7c207
Kia <kivi0001@stud.kea.dk>
2026-02-06 12:00:23 +0100

    rettelse på capitalization

--- capitalization3/capitalization3.js
+++ capitalization3/capitalization3.js
@@ -1,9 +1,9 @@
-/* const name = "Margrethe";
-const capName = `${name.substring(0, 2)}${name[2].toUpperCase()}${name.substring(3)}`;
-
-console.log(capName); */
-
-const name = "margrethe";
-const capName = `${name[0].toUpperCase()}${name.substring(1)}`;
+const name = "mArgREthE";
+const capName = `${name.substring(0, 2).toLowerCase()}${name[2].toUpperCase()}${name.substring(3).toLowerCase()}`;
 
 console.log(capName);
+
+/* const name = "margrethe";
+const capName = `${name[0].toUpperCase()}${name.substring(1)}`;
+
+console.log(capName); */
