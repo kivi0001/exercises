aa0dc897e18cd8f0d33ec1934a37341a19bb069c
Kia <kivi0001@stud.kea.dk>
2026-02-12 11:39:30 +0100

    sortby class css

--- animalbase/animalbase.js
+++ animalbase/animalbase.js
@@ -97,6 +97,13 @@ function selectSort(event){
     const sortBy = event.target.dataset.sort;
     const sortDir = event.target.dataset.sortDirection;
 
+    //find old sortBy element and remove sortBy
+    const oldElement = document.querySelector(`[data-sort='${settings.sortBy}']`);
+    oldElement.classList.remove("sortby");
+
+    //indicate active sort
+    event.target.classList.add("sortby");
+
     // toggle direction
     if (sortDir === "asc"){
         event.target.dataset.sortDirection = "desc";
